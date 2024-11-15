2355696895756928a31a4fcb5a00861489aa97b7
HDanielO <hameeddnl@gmail.com>
2024-11-15 09:01:38 +0100

    Learnt about Dynamic Values in Template

--- dojo-hod-blog/src/App.js
+++ dojo-hod-blog/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to the new blog by HOD";
+  const like = 50;
+  const person = true;
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>likes {like} times</p>
+        <p>{`Hello I am ${like} Cents`}</p>
       </div>
     </div>
   );
