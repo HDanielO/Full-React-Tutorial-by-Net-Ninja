199fc363dc529b01f0a0bdd87b776bab44469be7
HDanielO <hameeddnl@gmail.com>
2024-11-16 10:45:25 +0100

    Learnt about event listeners and click functions in React components

--- dojo-hod-blog/src/Home.js
+++ dojo-hod-blog/src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClickFirst = () => {
+    console.log("test value ");
+  };
+
+  const handleClick = () => {
+    console.log("test value 2 ");
+  };
+
+//So I noticed that you can't use the normal 'document.queryselector().addEventListener kind of way because we'll be referencing a DOM element in out queryselectot() method that we have not yet passed..because it comes before the return which passes the elements into tht DOM
+
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button onClick={handleClickFirst}>CLICK HERE</button>
+      <button onClick={() => handleClick()}>CLICK HERE AGAIN</button>
     </div>
   );
 };
