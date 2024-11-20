ee6502ddb14ff09d85314acb0fbe064c139fe700
HDanielO <hameeddnl@gmail.com>
2024-11-20 14:02:35 +0100

    Learnt about useEffect hook

--- dojo-hod-blog/src/Home.js
+++ dojo-hod-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -19,6 +19,10 @@ const Home = () => {
     });
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log("use effect ran");
+  });
 
   return (
     <div className="home">
