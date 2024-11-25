b846d175346f479adbcf2251e7a4b2f1c3f7f3b2
HDanielO <hameeddnl@gmail.com>
2024-11-25 15:10:04 +0100

    Learnt how to fetch data from a json server in useEffect() using fetch() & .then() method

--- dojo-hod-blog/src/BlogList.js
+++ dojo-hod-blog/src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogData, title, handleDelete }) => {
+const BlogList = ({ blogData, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -6,7 +6,6 @@ const BlogList = ({ blogData, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>delete blog</button>
         </div>
       ))}
     </div>
--- dojo-hod-blog/src/Home.js
+++ dojo-hod-blog/src/Home.js
@@ -2,40 +2,18 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => {
-      return blog.id !== id;
-    });
-    setBlogs(newBlogs);
-  };
-
-  const [name, setName] = useState("Mario");
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
     console.log("use effect ran");
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
   return (
     <div className="home">
-      <BlogList
-        blogData={blogs}
-        title="All BLOGS!"
-        handleDelete={handleDelete}
-      />
-      <button onClick={() => setName("Luigi")}>change name</button>
-      <p>{name}</p>
+      {blogs && <BlogList blogData={blogs} title="All BLOGS!" />}
     </div>
   );
 };
