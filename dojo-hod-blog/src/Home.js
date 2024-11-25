22854b94311129eba365250a04351a8cc33d979a
HDanielO <hameeddnl@gmail.com>
2024-11-25 16:34:44 +0100

    Learnt about conditional templates and how to create a loading state.

--- dojo-hod-blog/src/Home.js
+++ dojo-hod-blog/src/Home.js
@@ -3,16 +3,25 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  // create a state to track loading
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     console.log("use effect ran");
     fetch("http://localhost:8000/blogs")
       .then((res) => res.json())
-      .then((data) => setBlogs(data));
+      .then((data) => {
+        setIsPending(false);
+        setBlogs(data);
+      });
+
+    // NB: The setTimeout() method will run a function after an amount of time that we specify. It takes two arguments..the first one is the function we want to run and the second one is the time in ms without the unit
+    // SYNTAX : setTimeout(function,time)...setTimeout(() =>{}, 1000)
   }, []);
 
   return (
     <div className="home">
+      {isPending && <p>Loading...</p>}
       {blogs && <BlogList blogData={blogs} title="All BLOGS!" />}
     </div>
   );
