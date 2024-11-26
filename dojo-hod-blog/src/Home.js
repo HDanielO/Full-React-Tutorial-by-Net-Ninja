863ba1953c056725ba00996a9b4bc42ca8d5c1b3
HDanielO <hameeddnl@gmail.com>
2024-11-26 07:07:11 +0100

    Learnt how to handle errors (the two kind of errors) with .catch() and the throw Error() method

--- dojo-hod-blog/src/Home.js
+++ dojo-hod-blog/src/Home.js
@@ -6,22 +6,36 @@ const Home = () => {
   // create a state to track loading
   const [isPending, setIsPending] = useState(true);
 
+  // There are two types of error
+  // 1. Connection error - when out site can't even connect to the server. We can track this using .catch() method
+  const [connectionError, setConnectionError] = useState(null);
+
+  // 2. Server error - This is when a server sends an error. It might be due to wrong endpoint or denial of access
+
   useEffect(() => {
     console.log("use effect ran");
     fetch("http://localhost:8000/blogs")
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Cant Fetch Data From Server");
+        }
+        return res.json();
+      })
       .then((data) => {
         setIsPending(false);
         setBlogs(data);
+        setConnectionError(null);
+      })
+      .catch((err) => {
+        setConnectionError(err.message);
+        setIsPending(false);
       });
-
-    // NB: The setTimeout() method will run a function after an amount of time that we specify. It takes two arguments..the first one is the function we want to run and the second one is the time in ms without the unit
-    // SYNTAX : setTimeout(function,time)...setTimeout(() =>{}, 1000)
   }, []);
 
   return (
     <div className="home">
       {isPending && <p>Loading...</p>}
+      {connectionError && <p>{connectionError}</p>}
       {blogs && <BlogList blogData={blogs} title="All BLOGS!" />}
     </div>
   );
