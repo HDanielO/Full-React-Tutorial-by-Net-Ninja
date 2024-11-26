2b7a5aae6cd0f6e128277fcabaa7fb43136f628a
HDanielO <hameeddnl@gmail.com>
2024-11-26 15:53:25 +0100

    Learnt about Route Links and how to use them in our components

--- dojo-hod-blog/src/Navbar.js
+++ dojo-hod-blog/src/Navbar.js
@@ -1,13 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1 className="logoname">THE DOJO BLOG</h1>
       <ul className="links">
         <li>
-          <a href="/">Home</a>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <a href="/create">New Blog</a>
+          <Link to="/create">New Blog</Link>
         </li>
       </ul>
     </nav>
