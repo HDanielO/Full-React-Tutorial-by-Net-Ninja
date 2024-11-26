8543d0029684a7244fa0fdd188759a237284d1c1
HDanielO <hameeddnl@gmail.com>
2024-11-26 11:01:26 +0100

    Learnt how to create custom hooks that can be reused in our components

--- dojo-hod-blog/src/Home.js
+++ dojo-hod-blog/src/Home.js
@@ -1,42 +1,16 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  // create a state to track loading
-  const [isPending, setIsPending] = useState(true);
-
-  // There are two types of error
-  // 1. Connection error - when out site can't even connect to the server. We can track this using .catch() method
-  const [connectionError, setConnectionError] = useState(null);
-
-  // 2. Server error - This is when a server sends an error. It might be due to wrong endpoint or denial of access
-
-  useEffect(() => {
-    console.log("use effect ran");
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Cant Fetch Data From Server");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setIsPending(false);
-        setBlogs(data);
-        setConnectionError(null);
-      })
-      .catch((err) => {
-        setConnectionError(err.message);
-        setIsPending(false);
-      });
-  }, []);
+  const { data, isPending, connectionError } = useFetch(
+    "http://localhost:8000/blogs"
+  );
 
   return (
     <div className="home">
       {isPending && <p>Loading...</p>}
       {connectionError && <p>{connectionError}</p>}
-      {blogs && <BlogList blogData={blogs} title="All BLOGS!" />}
+      {data && <BlogList blogData={data} title="All BLOGS!" />}
     </div>
   );
 };
---
+++ dojo-hod-blog/src/useFetch.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  // create a state to track loading
+  const [isPending, setIsPending] = useState(true);
+
+  // There are two types of error
+  // 1. Connection error - when out site can't even connect to the server. We can track this using .catch() method
+  const [connectionError, setConnectionError] = useState(null);
+
+  // 2. Server error - This is when a server sends an error. It might be due to wrong endpoint or denial of access
+
+  useEffect(() => {
+    console.log("use effect ran");
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Cant Fetch Data From Server");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setIsPending(false);
+        setConnectionError(null);
+        setData(data);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setConnectionError(err.message);
+      });
+  }, []);
+
+  return { data, isPending, connectionError };
+};
+
+export default useFetch;
