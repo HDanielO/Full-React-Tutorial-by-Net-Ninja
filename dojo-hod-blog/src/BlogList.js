72a6d1d3b2382cab79bc334521b3dabe60f09346
HDanielO <hameeddnl@gmail.com>
2024-11-28 09:51:13 +0100

    Learnt how to create route variables and how to use it to link to a component but with different content based on the route variables

--- dojo-hod-blog/src/BlogList.js
+++ dojo-hod-blog/src/BlogList.js
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const BlogList = ({ blogData, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
       {blogData.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
+        <Link to={`/blogs/${blog.id}`}>
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
